4d022dc79ea6e8cb492d2c17a92cabcbb1e11f9e
Tausif Islam Sheik <mr.tausif904@gmail.com>
2026-02-23 05:18:54 +0600

    Create Zod Error Handler For Global Error Handler

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -3,6 +3,9 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import status from "http-status";
+import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
+import { handleZodError } from "../errorHelpers/handleZodError";
+import z from "zod";
 
 export const globalErrorHandler = (
   err: any,
@@ -14,12 +17,50 @@ export const globalErrorHandler = (
     console.log("Error from global error handler", err);
   }
 
-  const statusCode: number = status.INTERNAL_SERVER_ERROR;
-  const message: string = "Internal server error";
+  
+    let errorSources: TErrorSources[] = []
+    let statusCode: number = status.INTERNAL_SERVER_ERROR;
+    let message: string = 'Internal Server Error';
+    let stack: string | undefined = undefined;
 
-  res.status(statusCode).json({
-    success: false,
-    message: message,
-    error: err.message,
-  });
+   if (err instanceof z.ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode as number
+        message = simplifiedError.message
+        errorSources = [...simplifiedError.errorSources]
+        stack = err.stack;
+   }
+    // } else if (err instanceof AppError) {
+    //     statusCode = err.statusCode;
+    //     message = err.message;
+    //     stack = err.stack;
+    //     errorSources = [
+    //         {
+    //             path: '',
+    //             message: err.message
+    //         }
+    //     ]
+    // }
+    // else if (err instanceof Error) {
+    //     statusCode = status.INTERNAL_SERVER_ERROR;
+    //     message = err.message
+    //     stack = err.stack;
+    //     errorSources = [
+    //         {
+    //             path: '',
+    //             message: err.message
+    //         }
+    //     ]
+    // }
+
+
+    const errorResponse: TErrorResponse = {
+        success: false,
+        message: message,
+        errorSources,
+        error: envVars.NODE_ENV === 'development' ? err : undefined,
+        stack: envVars.NODE_ENV === 'development' ? stack : undefined,
+    }
+
+    res.status(statusCode).json(errorResponse);
 };
