6844cc0adc11627666a33b91b55b0dad84c4f071
Tausif Islam Sheik <mr.tausif904@gmail.com>
2026-02-24 00:53:11 +0600

    Create Authorization Middleware & Protecting for Speciality Routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import { IndexRoutes } from "./app/routes";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import { notFound } from "./app/middleware/notFound";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/api/v1", IndexRoutes);
 
--- src/app/module/speciality/speciality.route.ts
+++ src/app/module/speciality/speciality.route.ts
@@ -1,12 +1,25 @@
 import { Router } from "express";
 import { specialityController } from "./speciality.controller";
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
 router.get("/", specialityController.getAllSpecialities);
-router.post("/", specialityController.createSpeciality);
-router.patch("/:id", specialityController.updateSpeciality);
-router.delete("/:id", specialityController.deleteSpeciality);
-
+router.post(
+  "/",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  specialityController.createSpeciality,
+);
+router.patch(
+  "/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  specialityController.updateSpeciality,
+);
+router.delete(
+  "/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  specialityController.deleteSpeciality,
+);
 
 export const specialityRoutes = router;
