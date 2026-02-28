8e2cb996e29b222e9cbdf5395e052634b288c97d
Tausif Islam Sheik <mr.tausif904@gmail.com>
2026-03-01 00:04:43 +0600

    Create CORS & Better Auth Trusted Origins

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -133,6 +133,15 @@ export const auth = betterAuth({
     },
   },
 
+  redirectURLs: {
+    signIn: `${envVars.BETTER_AUTH_URL}/api/v1/auth/google/success`,
+  },
+
+  trustedOrigins: [
+    process.env.BETTER_AUTH_URL || "http://localhost:5000",
+    envVars.FRONTEND_URL,
+  ],
+
   advanced: {
     // disableCSRFCheck: true,
     useSecureCookies: false,
