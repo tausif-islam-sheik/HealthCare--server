1e6912beada1ba0eb37d3c206750d105ac070d1d
Tausif Islam Sheik <mr.tausif904@gmail.com>
2026-03-02 16:18:43 +0600

    Create Filter Method For Query Builder

--- src/app/utils/QueryBuilder.ts
+++ src/app/utils/QueryBuilder.ts
@@ -106,4 +106,146 @@ export class QueryBuilder<
 
     return this;
   }
+
+  // /doctors?searchTerm=john&page=1&sortBy=name&specialty=cardiology&appointmentFee[lt]=100 => {}
+  // { specialty: 'cardiology', appointmentFee: { lt: '100' } }
+  filter(): this {
+    const { filterableFields } = this.config;
+    const excludedField = [
+      "searchTerm",
+      "page",
+      "limit",
+      "sortBy",
+      "sortOrder",
+      "fields",
+      "include",
+    ];
+
+    const filterParams: Record<string, unknown> = {};
+
+    Object.keys(this.queryParams).forEach((key) => {
+      if (!excludedField.includes(key)) {
+        filterParams[key] = this.queryParams[key];
+      }
+    });
+
+    const queryWhere = this.query.where as Record<string, unknown>;
+    const countQueryWhere = this.countQuery.where as Record<string, unknown>;
+
+    Object.keys(filterParams).forEach((key) => {
+      const value = filterParams[key];
+
+      if (value === undefined || value === "") {
+        return;
+      }
+
+      const isAllowedField =
+        !filterableFields ||
+        filterableFields.length === 0 ||
+        filterableFields.includes(key);
+
+      // doctorFilterableFields = ['specialties.specialty.title', 'appointmentFee']
+      // /doctors?appointmentFee[lt]=100&appointmentFee[gt]=50 => { appointmentFee: { lt: '100', gt: '50' } }
+
+      // /doctors?user.name=John => { user: { name: 'John' } }
+      if (key.includes(".")) {
+        const parts = key.split(".");
+
+        if (filterableFields && !filterableFields.includes(key)) {
+          return;
+        }
+
+        if (parts.length === 2) {
+          const [relation, nestedField] = parts;
+
+          if (!queryWhere[relation]) {
+            queryWhere[relation] = {};
+            countQueryWhere[relation] = {};
+          }
+
+          const queryRelation = queryWhere[relation] as Record<string, unknown>;
+          const countRelation = countQueryWhere[relation] as Record<
+            string,
+            unknown
+          >;
+
+          queryRelation[nestedField] = this.parseFilterValue(value);
+          countRelation[nestedField] = this.parseFilterValue(value);
+          return;
+        } else if (parts.length === 3) {
+          const [relation, nestedRelation, nestedField] = parts;
+
+          if (!queryWhere[relation]) {
+            queryWhere[relation] = {
+              some: {},
+            };
+            countQueryWhere[relation] = {
+              some: {},
+            };
+          }
+
+          const queryRelation = queryWhere[relation] as Record<string, unknown>;
+          const countRelation = countQueryWhere[relation] as Record<
+            string,
+            unknown
+          >;
+
+          if (!queryRelation.some) {
+            queryRelation.some = {};
+          }
+          if (!countRelation.some) {
+            countRelation.some = {};
+          }
+
+          const querySome = queryRelation.some as Record<string, unknown>;
+          const countSome = countRelation.some as Record<string, unknown>;
+
+          if (!querySome[nestedRelation]) {
+            querySome[nestedRelation] = {};
+          }
+
+          if (!countSome[nestedRelation]) {
+            countSome[nestedRelation] = {};
+          }
+
+          const queryNestedRelation = querySome[nestedRelation] as Record<
+            string,
+            unknown
+          >;
+          const countNestedRelation = countSome[nestedRelation] as Record<
+            string,
+            unknown
+          >;
+
+          queryNestedRelation[nestedField] = this.parseFilterValue(value);
+          countNestedRelation[nestedField] = this.parseFilterValue(value);
+
+          return;
+        }
+      }
+      if (!isAllowedField) {
+        return;
+      }
+
+      // Range filter parsing
+      if (
+        typeof value === "object" &&
+        value !== null &&
+        !Array.isArray(value)
+      ) {
+        queryWhere[key] = this.parseRangeFilter(
+          value as Record<string, string | number>,
+        );
+        countQueryWhere[key] = this.parseRangeFilter(
+          value as Record<string, string | number>,
+        );
+        return;
+      }
+
+      //direct value parsing
+      queryWhere[key] = this.parseFilterValue(value);
+      countQueryWhere[key] = this.parseFilterValue(value);
+    });
+    return this;
+  }
 }
